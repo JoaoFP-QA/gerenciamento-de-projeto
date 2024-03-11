eeb098afec4d1b5b861acaf0fcafe08b4163f849
JoaoFP-QA <joaofabiopinto2016@gmail.com>
2024-03-11 19:26:11 -0300

    Iniciando a page Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,15 @@
+import styles from './Home.module.css'
+import icon_din from '../../img/icon_din.png'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Gerenciador de Projetos</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href='/'>Criar Projeto</a>
+            <img src={icon_din} alt="Gerenciador de Projeto" />
+        </section>
+    )
 }
 
 export default Home
