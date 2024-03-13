94f01c7ac184c5642b148131c5b5e73b387c4fca
JoaoFP-QA <joaofabiopinto2016@gmail.com>
2024-03-13 20:28:31 -0300

    Acertando a inclusão dos dados no sistema

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,40 @@
+import { useHistory } from 'react-router-dom'
 import ProjectsForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
+
+    const history = useHistory()
+
+    function createPost(project) {
+
+        // inicialize o gerenciador and services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'content-Type': 'aplication/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then((resp) => resp.json()
+        .then((data) => {
+            console.log(data)
+            //redirect
+            history.push('/projects', {messege: 'Projeto Criado com sucesso!'})
+        })
+        .catch((err) => console.log(err))
+        )
+        
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectsForm  btnText="Criar Projeto"/>
+            <ProjectsForm handleSubmit={createPost}  btnText="Criar Projeto"/>
         </div>
     )
 }
